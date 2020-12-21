eea2e2d106bb51fdb8b655c0d801ddb358d44771
Lukas Rohner <lukas.rohner@valtech.com>
2020-12-21 17:58:44 +0100

    Added gzip compression

--- kubernetes-demo-frontend/gzipAssets.sh
+++ kubernetes-demo-frontend/gzipAssets.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#gzip -fk /var/www/html/kubernetes-demo-frontend/scripts/*.js
-#gzip -fk /var/www/html/kubernetes-demo-frontend/*.css
+gzip -fk /var/www/html/kubernetes-demo-frontend/static/js/*.js
+gzip -fk /var/www/html/kubernetes-demo-frontend/static/css/*.css
